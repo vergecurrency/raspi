8650fe86a2d6461783dbb547a85bbc41a0e15414
badbrainIRC <egypte333@hotmail.com>
2017-07-21 07:25:51 -0400

    Update go.sh

--- go.sh
+++ go.sh
@@ -12,9 +12,23 @@ sudo add-apt-repository ppa:bitcoin/bitcoin
 
 sudo apt-get update
 
+sudo apt-get install libcanberra-gtk-module
+
 sudo apt-get -y install git libdb4.8-dev libdb4.8++-dev
 
-sudo apt-get -y install git build-essential libtool autotools-dev autoconf automake pkg-config libssl-dev libevent-dev bsdmainutils git libboost-all-dev libminiupnpc-dev libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev
+sudo apt-get -y install git build-essential libtool autotools-dev autoconf automake pkg-config libssl-dev libevent-dev bsdmainutils git libprotobuf-dev protobuf-compiler libqrencode-dev
+
+sudo apt-get -y install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools
+
+sudo apt-get -y install libminiupnpc-dev
+
+sudo apt-get -y install libboost-all-dev
+
+sudo apt-get -y install --no-install-recommends gnome-panel
+
+sudo apt-get -y install lynx
+
+sudo apt-get -y install unzip
 
 #// Compile Berkeley
 cd ~
@@ -23,7 +37,6 @@ tar -xzvf db-4.8.30.NC.tar.gz
 rm db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix 
 ../dist/configure --enable-cxx 
-make clean
 make 
 sudo make install 
 sudo ln -s /usr/local/BerkeleyDB.4.8/lib/libdb-4.8.so /usr/lib/libdb-4.8.so
@@ -38,7 +51,6 @@ sudo sh autogen.sh
 chmod 777 ~/raspi/share/genbuild.sh
 chmod 777 ~/raspi/src/leveldb/build_detect_platform
 cd ~/raspi 
-make clean 
 ./configure CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O2" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --with-boost-libdir=/usr/lib/arm-linux-gnueabihf --with-gui=qt5 --disable-sse2 
 make 
 sudo strip ~/raspi/src/VERGEd
@@ -53,17 +65,14 @@ echo "rpcuser="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 26 ; echo '') '\
 
 #// Extract http link, download blockchain and install it.
 
-sudo apt-get -y install lynx
 echo "wget" $(lynx --dump --listonly http://vergecurrency.de | grep -o "http:*.*zip") > link.sh
 sh link.sh
-sudo apt-get -y install unzip
 unzip -o Verge-Blockchain*.zip -d ~/.VERGE
 sudo rm Verge-Blockchain*.zip
 
 # Create Icon on Desktop and in menu
 
 sudo cp ~/raspi/vergepi.png /usr/share/icons/
-sudo apt-get -y install --no-install-recommends gnome-panel
 echo '#!/usr/bin/env xdg-open''\n'"[Desktop Entry]"'\n'"Version=1.0"'\n'"Type=Application"'\n'"Terminal=false"'\n'"Icon[en]=/usr/share/icons/vergepi.png"'\n'"Name[en]=VERGE"'\n'"Exec=VERGE-qt"'\n'"Name=VERGE"'\n'"Icon=/usr/share/icons/vergepi.png"'\n'"Categories=Network;Internet;" > ~/Desktop/VERGE.desktop
 sudo chmod +x ~/Desktop/VERGE.desktop
 sudo cp ~/Desktop/VERGE.desktop /usr/share/applications/VERGE.desktop
